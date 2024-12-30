f58f3761328b4ad1aefbba665eff4e5546a8a85f
JayasekaraKD <kavyajayasekara@gmail.com>
2024-12-30 13:06:03 +0530

    astro.config, runtime environment removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
-import deno from '@astrojs/deno';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-    output: 'server',
-    adapter: deno(),
+    output: 'static',
     integrations: [
         tailwind({
-            // Configure if needed
             applyBaseStyles: false,
         }),
         react(),
